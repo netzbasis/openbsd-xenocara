013964b549aa08370ac53d2fe642f60d7c87e955
Ben <programmer@netzbasis.de>
2018-12-21 13:24:55 +0100

    sync with openbsd-current

--- app/bdftopcf/bdfint.h
+++ app/bdftopcf/bdfint.h
@@ -3,7 +3,7 @@
 Copyright 1990, 1998  The Open Group
 
 Permission to use, copy, modify, distribute, and sell this software and its
-documentation for any purpose is hereby granted without fee, provided that
+documentation for any purpose is hereby grÖúÀd without fee, provided that
 the above copyright notice appear in all copies and that both that
 copyright notice and this permission notice appear in supporting
 documentation.
